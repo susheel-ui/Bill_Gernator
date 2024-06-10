37957cf7952d529cf6986ecbcc974d206a9a4de3
susheel-ui <susheelverma444@gmail.com>
2024-06-10 14:54:48 +0530

    view Activity code ux

--- app/src/main/java/com/example/bill_genrating_app/ViewItemActivity.kt
+++ app/src/main/java/com/example/bill_genrating_app/ViewItemActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.bill_genrating_app.Roomdb.DBHelper
+import com.example.bill_genrating_app.Roomdb.entities.items
 import com.example.bill_genrating_app.databinding.ActivityViewItemBinding
 
 class ViewItemActivity : AppCompatActivity() {
@@ -28,7 +32,39 @@ class ViewItemActivity : AppCompatActivity() {
         super.onStart()
         val barcodeid= intent.getStringExtra("itemId")
         //this is for only test purpose
-        thisPageBinding.textview.text = barcodeid.toString()
 
+        val item = barcodeid?.let { fetchData(it.toLong()) }
+        thisPageBinding.itemname.text = item?.Name.toString()
+        thisPageBinding.itemMRP.text = item?.MRP.toString()
+        thisPageBinding.itemWeight.text = item?.weight.toString().plus(item?.weightType.toString())
+        thisPageBinding.itemCatagory.text = item?.Type.toString()
+        thisPageBinding.itemQuntity.setText(item?.stockQuantity.toString())
+
+        thisPageBinding.btnminus.setOnClickListener {
+            var Quntity = thisPageBinding.itemQuntity.text.toString().toLong()
+            if(Quntity.toInt() != 0){
+                Quntity -= 1;
+            }
+            thisPageBinding.itemQuntity.setText(Quntity.toString())
+        }
+        thisPageBinding.btnadd.setOnClickListener {
+            var Quntity = thisPageBinding.itemQuntity.text.toString().toLong()
+            thisPageBinding.itemQuntity.setText((Quntity + 1).toString())
+        }
+
+    }
+    public fun fetchDb():DBHelper{
+        val db = Room.databaseBuilder(
+            applicationContext,
+            DBHelper::class.java
+            ,"DatabaseBillGenerator"
+            ).allowMainThreadQueries().fallbackToDestructiveMigration().build()
+        return db;
+    }
+
+    public fun fetchData(barcode:Long):items{
+        val db = fetchDb();
+        val item = db.itemDao().getByid(barcode)
+        return item.get(0);
     }
 }
--- app/src/main/java/com/example/bill_genrating_app/items_fragment.kt
+++ app/src/main/java/com/example/bill_genrating_app/items_fragment.kt
@@ -69,8 +69,6 @@ class items_fragment : Fragment() {
 
 
         return thisFagementBinding.root
-
-
     }
 
     override fun onStart() {
