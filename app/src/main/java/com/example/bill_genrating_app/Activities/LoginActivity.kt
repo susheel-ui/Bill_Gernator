8757b663378277957d717f3bdffd5d4c695d9cef
susheel-ui <susheelverma444@gmail.com>
2024-09-15 01:38:45 +0530

    login Page add

---
+++ app/src/main/java/com/example/bill_genrating_app/Activities/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bill_genrating_app.Activities
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bill_genrating_app.R
+import com.example.bill_genrating_app.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    var ActivityBinding:ActivityLoginBinding ?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(ActivityBinding?.root)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+        return super.onCreateView(name, context, attrs)
+    }
+}
