613edd2ef707fe19995833cbd37759b7d562ce3b
Susheel Verma <72841849+susheel-ui@users.noreply.github.com>
2025-04-21 23:29:52 +0530

    UI color pallate change

--- app/src/main/java/com/example/bill_genrating_app/Activities/MainActivity.kt
+++ app/src/main/java/com/example/bill_genrating_app/Activities/MainActivity.kt
@@ -14,40 +14,38 @@ import com.example.bill_genrating_app.Fragments.*
 
 class MainActivity : AppCompatActivity() {
 
-   lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // binding for current activity
         binding = ActivityMainBinding.inflate(layoutInflater)
-        change_fragment(invoice_fragment(),"invoices")
+        change_fragment(invoice_fragment(), "invoices")
         setContentView(binding.root)
-
-
 
 
         // all listeners is here
 
-            // fragement changing Actions
+        // fragement changing Actions
         this.binding.bottomNavBarlayout.itemOne.setOnClickListener {
-            change_fragment(invoice_fragment(),"invoices")
+            change_fragment(invoice_fragment(), "invoices")
         }
         this.binding.bottomNavBarlayout.itemTwo.setOnClickListener {
-            change_fragment(clients_fragments(),"clients")
+            change_fragment(items_fragment(), "Item")
         }
         this.binding.bottomNavBarlayout.itemthree.setOnClickListener {
-            change_fragment(items_fragment(),"clients")
+            change_fragment(clients_fragments(), "clients")
         }
         this.binding.bottomNavBarlayout.itemFour.setOnClickListener {
-            change_fragment(setting_fragment(),"setting")
+            change_fragment(setting_fragment(), "setting")
         }// Note: here itemOne,itemTwo,itemThree,itemFour is basically menu items because its a custom bottom navbar
 
 //         add order button listner
-            this.binding.bottomNavBarlayout.NewOrderbtn.setOnClickListener {
-                val intent = Intent(applicationContext,OrderActivity::class.java)
-                startActivity(intent)
-            }
+        this.binding.bottomNavBarlayout.NewOrderbtn.setOnClickListener {
+            val intent = Intent(applicationContext, OrderActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
@@ -56,9 +54,10 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         // here we do code for the
     }
-    private fun change_fragment(Fg:Fragment,pagename:String){
-        var manager:FragmentManager = supportFragmentManager
-       manager.beginTransaction().replace(R.id.Container_view,Fg).commit()
+
+    private fun change_fragment(Fg: Fragment, pagename: String) {
+        var manager: FragmentManager = supportFragmentManager
+        manager.beginTransaction().replace(R.id.Container_view, Fg).commit()
 
     }
 }
